f355aae1d5b74b28ac2044fd85b146537b118104
Moloko Percy Rakgoale <moloko.rakgoale@capaciti.org.za>
2025-02-05 05:44:35 -0600

    Updating vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
  server: {
     host: true,
     port: process.env.PORT || 5173,
-    allowedHosts: ['movie-database-smcf.onrender.com'],
+    allowedHosts: ['code-crafter-moviedatabase-41yc.onrender.com'],
   },
 });
